f949f271fb8bf5576e92e5bdb58c30627649d9a3
Dixit Gogoi <dixitgogoi2017@gmail.com>
2023-08-02 23:35:33 +0530

    Featured section added

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -2,25 +2,167 @@ import Image from "next/image";
 
 const Featured = () => {
   return (
-    <div className="w-screen">
-      <div className="w-[100vw] flex bg-red-600">
-        <div className="w-screen h-[50vh] flex flex-col items-center justify-around p-3">
+    <div className="w-screen overflow-x-scroll text-green-500">
+      <div className="w-max flex">
+        <div className="w-screen sm:w-[50vw] md:w-[33vw] lg:w-[25vw] h-[60vh] md:h-[70vh] flex flex-col justify-around items-center p-4 hover:bg-fuchsia-50 transition-all duration-150">
 
-          <div className="w-full relative flex-1">
-            <Image src="/temporary/p1.png" fill className="object-contain" />
+          <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-300">
+            <Image src="/temporary/p1.png" fill={true} className="object-contain" />
           </div>
-
-          <div className="flex flex-col gap-2 flex-1">
-            <h1>Title</h1>
-            <p>desc</p>
-            <span>1250</span>
-            <button>Add to cart</button>
+          <div className="flex-1 flex flex-col gap-4 justify-center items-center">
+            <h1 className="text-lg font-bold uppercase">title</h1>
+            <p className="text-center px-5 font-medium leading-snug">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi dolorum doloribus delectus exercitationem</p>
+            <span className="font-bold md:text-lg">₹1233</span>
+            <button className="bg-green-500 text-white rounded-sm py-1 px-3">Add to cart</button>
           </div>
 
         </div>
+        <div className="w-screen sm:w-[50vw] md:w-[33vw] lg:w-[25vw] h-[60vh] md:h-[70vh] flex flex-col justify-around items-center p-4 hover:bg-fuchsia-50 transition-all duration-150">
+
+          <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-300">
+            <Image src="/temporary/p2.png" fill={true} className="object-contain" />
+          </div>
+          <div className="flex-1 flex flex-col gap-4 justify-center items-center">
+            <h1 className="text-lg font-bold uppercase">title</h1>
+            <p className="text-center px-5 font-medium leading-snug">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi dolorum doloribus delectus exercitationem</p>
+            <span className="font-bold md:text-lg">₹1233</span>
+            <button className="bg-green-500 text-white rounded-sm py-1 px-3">Add to cart</button>
+          </div>
+
+        </div>
+        <div className="w-screen sm:w-[50vw] md:w-[33vw] lg:w-[25vw] h-[60vh] md:h-[70vh] flex flex-col justify-around items-center p-4 hover:bg-fuchsia-50 transition-all duration-150">
+
+          <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-300">
+            <Image src="/temporary/p3.png" fill={true} className="object-contain" />
+          </div>
+          <div className="flex-1 flex flex-col gap-4 justify-center items-center">
+            <h1 className="text-lg font-bold uppercase">title</h1>
+            <p className="text-center px-5 font-medium leading-snug">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi dolorum doloribus delectus exercitationem</p>
+            <span className="font-bold md:text-lg">₹1233</span>
+            <button className="bg-green-500 text-white rounded-sm py-1 px-3">Add to cart</button>
+          </div>
+
+        </div>
+        <div className="w-screen sm:w-[50vw] md:w-[33vw] lg:w-[25vw] h-[60vh] md:h-[70vh] flex flex-col justify-around items-center p-4 hover:bg-fuchsia-50 transition-all duration-150">
+
+          <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-300">
+            <Image src="/temporary/p4.png" fill={true} className="object-contain" />
+          </div>
+          <div className="flex-1 flex flex-col gap-4 justify-center items-center">
+            <h1 className="text-lg font-bold uppercase">title</h1>
+            <p className="text-center px-5 font-medium leading-snug">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi dolorum doloribus delectus exercitationem</p>
+            <span className="font-bold md:text-lg">₹1233</span>
+            <button className="bg-green-500 text-white rounded-sm py-1 px-3">Add to cart</button>
+          </div>
+
+        </div>
+        <div className="w-screen sm:w-[50vw] md:w-[33vw] lg:w-[25vw] h-[60vh] md:h-[70vh] flex flex-col justify-around items-center p-4 hover:bg-fuchsia-50 transition-all duration-150">
+
+          <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-300">
+            <Image src="/temporary/p5.png" fill={true} className="object-contain" />
+          </div>
+          <div className="flex-1 flex flex-col gap-4 justify-center items-center">
+            <h1 className="text-lg font-bold uppercase">title</h1>
+            <p className="text-center px-5 font-medium leading-snug">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi dolorum doloribus delectus exercitationem</p>
+            <span className="font-bold md:text-lg">₹1233</span>
+            <button className="bg-green-500 text-white rounded-sm py-1 px-3">Add to cart</button>
+          </div>
+
+        </div>
+        <div className="w-screen sm:w-[50vw] md:w-[33vw] lg:w-[25vw] h-[60vh] md:h-[70vh] flex flex-col justify-around items-center p-4 hover:bg-fuchsia-50 transition-all duration-150">
+
+          <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-300">
+            <Image src="/temporary/p6.png" fill={true} className="object-contain" />
+          </div>
+          <div className="flex-1 flex flex-col gap-4 justify-center items-center">
+            <h1 className="text-lg font-bold uppercase">title</h1>
+            <p className="text-center px-5 font-medium leading-snug">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi dolorum doloribus delectus exercitationem</p>
+            <span className="font-bold md:text-lg">₹1233</span>
+            <button className="bg-green-500 text-white rounded-sm py-1 px-3">Add to cart</button>
+          </div>
+
+        </div>
+        <div className="w-screen sm:w-[50vw] md:w-[33vw] lg:w-[25vw] h-[60vh] md:h-[70vh] flex flex-col justify-around items-center p-4 hover:bg-fuchsia-50 transition-all duration-150">
+
+          <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-300">
+            <Image src="/temporary/p7.png" fill={true} className="object-contain" />
+          </div>
+          <div className="flex-1 flex flex-col gap-4 justify-center items-center">
+            <h1 className="text-lg font-bold uppercase">title</h1>
+            <p className="text-center px-5 font-medium leading-snug">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi dolorum doloribus delectus exercitationem</p>
+            <span className="font-bold md:text-lg">₹1233</span>
+            <button className="bg-green-500 text-white rounded-sm py-1 px-3">Add to cart</button>
+          </div>
+
+        </div>
+        <div className="w-screen sm:w-[50vw] md:w-[33vw] lg:w-[25vw] h-[60vh] md:h-[70vh] flex flex-col justify-around items-center p-4 hover:bg-fuchsia-50 transition-all duration-150">
+
+          <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-300">
+            <Image src="/temporary/p8.png" fill={true} className="object-contain" />
+          </div>
+          <div className="flex-1 flex flex-col gap-4 justify-center items-center">
+            <h1 className="text-lg font-bold uppercase">title</h1>
+            <p className="text-center px-5 font-medium leading-snug">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi dolorum doloribus delectus exercitationem</p>
+            <span className="font-bold md:text-lg">₹1233</span>
+            <button className="bg-green-500 text-white rounded-sm py-1 px-3">Add to cart</button>
+          </div>
+
+        </div>
+        <div className="w-screen sm:w-[50vw] md:w-[33vw] lg:w-[25vw] h-[60vh] md:h-[70vh] flex flex-col justify-around items-center p-4 hover:bg-fuchsia-50 transition-all duration-150">
+
+          <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-300">
+            <Image src="/temporary/p9.png" fill={true} className="object-contain" />
+          </div>
+          <div className="flex-1 flex flex-col gap-4 justify-center items-center">
+            <h1 className="text-lg font-bold uppercase">title</h1>
+            <p className="text-center px-5 font-medium leading-snug">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi dolorum doloribus delectus exercitationem</p>
+            <span className="font-bold md:text-lg">₹1233</span>
+            <button className="bg-green-500 text-white rounded-sm py-1 px-3">Add to cart</button>
+          </div>
+
+        </div>
+        <div className="w-screen sm:w-[50vw] md:w-[33vw] lg:w-[25vw] h-[60vh] md:h-[70vh] flex flex-col justify-around items-center p-4 hover:bg-fuchsia-50 transition-all duration-150">
+
+          <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-300">
+            <Image src="/temporary/p10.png" fill={true} className="object-contain" />
+          </div>
+          <div className="flex-1 flex flex-col gap-4 justify-center items-center">
+            <h1 className="text-lg font-bold uppercase">title</h1>
+            <p className="text-center px-5 font-medium leading-snug">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi dolorum doloribus delectus exercitationem</p>
+            <span className="font-bold md:text-lg">₹1233</span>
+            <button className="bg-green-500 text-white rounded-sm py-1 px-3">Add to cart</button>
+          </div>
+
+        </div>
+        <div className="w-screen sm:w-[50vw] md:w-[33vw] lg:w-[25vw] h-[60vh] md:h-[70vh] flex flex-col justify-around items-center p-4 hover:bg-fuchsia-50 transition-all duration-150">
+
+          <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-300">
+            <Image src="/temporary/p11.png" fill={true} className="object-contain" />
+          </div>
+          <div className="flex-1 flex flex-col gap-4 justify-center items-center">
+            <h1 className="text-lg font-bold uppercase">title</h1>
+            <p className="text-center px-5 font-medium leading-snug">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi dolorum doloribus delectus exercitationem</p>
+            <span className="font-bold md:text-lg">₹1233</span>
+            <button className="bg-green-500 text-white rounded-sm py-1 px-3">Add to cart</button>
+          </div>
+
+        </div>
+        <div className="w-screen sm:w-[50vw] md:w-[33vw] lg:w-[25vw] h-[60vh] md:h-[70vh] flex flex-col justify-around items-center p-4 hover:bg-fuchsia-50 transition-all duration-150">
+
+          <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-300">
+            <Image src="/temporary/p12.png" fill={true} className="object-contain" />
+          </div>
+          <div className="flex-1 flex flex-col gap-4 justify-center items-center">
+            <h1 className="text-lg font-bold uppercase">title</h1>
+            <p className="text-center px-5 font-medium leading-snug">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi dolorum doloribus delectus exercitationem</p>
+            <span className="font-bold md:text-lg">₹1233</span>
+            <button className="bg-green-500 text-white rounded-sm py-1 px-3">Add to cart</button>
+          </div>
+
+        </div>
+   
       </div>
     </div>
-    
   );
 }
 
